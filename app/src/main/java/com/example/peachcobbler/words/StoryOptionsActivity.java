6217a2365da9a9a056066466d43dc6088b392a01
seangovens <seangovens@gmail.com>
2016-09-17 00:44:08 -0600

    Ayyyyy

--- app/src/main/java/com/example/peachcobbler/words/StoryOptionsActivity.java
+++ app/src/main/java/com/example/peachcobbler/words/StoryOptionsActivity.java
@@ -1,25 +1,32 @@
 package com.example.peachcobbler.words;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class StoryOptionsActivity extends AppCompatActivity {
 
-    private int wordCount = 1;
+    private String user;
+    private int wordCount = 1, wordMax = 10;
+    private final int[] MAXES = new int[] {20, 50, 100, 200, 500, 1000, 2500, 5000, 10000, 50000};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_options);
 
-        SeekBar bar = (SeekBar) findViewById(R.id.wordAmountBar);
-        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        Intent intent = getIntent();
+        user = intent.getStringExtra("user");
+
+        SeekBar wordCountBar = (SeekBar) findViewById(R.id.wordAmountBar);
+        wordCountBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
                 wordCount = i + 1;
-                ((TextView) findViewById(R.id.wordNum)).setText(String.valueOf(i + 1));
+                ((TextView) findViewById(R.id.wordNum)).setText(String.valueOf(wordCount));
             }
 
             @Override
@@ -32,5 +39,35 @@ public class StoryOptionsActivity extends AppCompatActivity {
                 return;
             }
         });
+
+        SeekBar wordMaxBar = (SeekBar) findViewById(R.id.wordMaxBar);
+        wordMaxBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                wordMax = MAXES[i];
+                ((TextView) findViewById(R.id.wordMax)).setText(String.valueOf(wordMax));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                return;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                return;
+            }
+        });
+    }
+
+    public void createStory(View view) {
+        // NEED TO ADD STORY IN DATABASE
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra("user", user);
+        startActivity(intent);
+    }
+
+    public void cancel(View view) {
+        finish();
     }
 }
